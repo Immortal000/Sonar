18367f422c35f6c3a59c55125955d6e3fde1b30a
siddarth kotthur <kotthursiddarth@gmail.com>
2023-07-11 08:54:24 -0500

    additional stores

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,17 @@ export const authStore = writeable({
   user: null,
   loading: true,
   data: null,
-});
+}); 
+
+export const uniStore = writeable({
+    universityName: null, 
+    universityCourses: [],
+})
+
+// will have to implement paginations for posts
+export const postsStore = writeable({
+    posts: [],
+})
 
 export const authHandler => { 
     signup: async (userID) => {
