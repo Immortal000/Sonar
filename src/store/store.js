21d4bdd3f4d6d9d7d9bbd9365a5d4e05d0ebf42e
siddarth kotthur <kotthursiddarth@gmail.com>
2023-07-11 17:09:12 -0500

    fix syntax error

--- src/store/store.js
+++ src/store/store.js
@@ -5,26 +5,26 @@ export const authStore = writeable({
   user: null,
   loading: true,
   data: null,
-}); 
+});
 
 export const uniStore = writeable({
-    universityName: null, 
-    universityCourses: [],
-})
+  universityName: null,
+  universityCourses: [],
+});
 
 // will have to implement paginations for posts
 export const postsStore = writeable({
-    posts: [],
-})
+  posts: [],
+});
 
-export const authHandler => { 
-    signup: async (userID) => {
-        await signupWithGoogle(userID)
-    },
-    login: async (userID) => {
-        await signinWithGoogle(userID)
-    },
-    logout: async () => {
-        await signout(auth)
-    }
-}
+export const authHandler = {
+  signup: async (userID) => {
+    await signupWithGoogle(userID);
+  },
+  login: async (userID) => {
+    await signinWithGoogle(userID);
+  },
+  logout: async () => {
+    await signout(auth);
+  },
+};
