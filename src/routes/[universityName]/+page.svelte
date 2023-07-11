12d650b50a24e11e3ac993b2af75f537ff7a8677
siddarth kotthur <kotthursiddarth@gmail.com>
2023-07-10 19:39:40 -0500

    Added tailwind support

---
+++ src/routes/[universityName]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  // university website
+  // import tailwind styles
+  import "../../styles/uni.css";
+
+  // page params
+  import { page } from "$app/stores";
+  const university = $page.params.universityName;
+</script>
+
+<div class="container">
+  <div class="toolbar" />
+</div>
---
+++ src/routes/[universityName]/[universityCourse]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  // university course
+  import { page } from "$app/stores";
+  const course = $page.params.universityCourse;
+</script>
+
+<h1>Current Course: {course}</h1>
