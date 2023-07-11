c8d2327f7dd61028cc4188aa19f9c7d401a9619f
WarrenWu4 <warrenweiwu04@gmail.com>
2023-07-10 19:45:48 -0500

    configuration settings updated

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 export default defineConfig({
 	plugins: [sveltekit()]
