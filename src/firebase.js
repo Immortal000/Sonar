d6887c56db5e0ade3cf9efcd13af5845c1bfc824
siddarth kotthur <kotthursiddarth@gmail.com>
2023-07-11 08:52:09 -0500

    Added store

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,30 @@
-import { initializeApp } from "firebase/app";
+import { getApp, deleteApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.APP_ID,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_APP_ID,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-export const storage = getStorage(app);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export default app;
+// init firebase app
+let firebaseApp;
+if (!getApps().length) {
+  firebaseApp = initializeApp(firebaseConfig);
+} else {
+  firebaseApp = getApp();
+  deleteApp(firebaseApp);
+  firebaseApp = initializeApp(firebaseConfig);
+}
+
+export const storage = getStorage(firebaseApp);
+export const db = getFirestore(firebaseApp);
+export const auth = getAuth(firebaseApp);
+export default firebaseApp;
---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import { writeable } from "svelte/store";
+import { auth } from "../firebase";
+
+export const authStore = writeable({
+  user: null,
+  loading: true,
+  data: null,
+});
+
+export const authHandler => { 
+    signup: async (userID) => {
+        await signupWithGoogle(userID)
+    },
+    login: async (userID) => {
+        await signinWithGoogle(userID)
+    },
+    logout: async () => {
+        await signout(auth)
+    }
+}
