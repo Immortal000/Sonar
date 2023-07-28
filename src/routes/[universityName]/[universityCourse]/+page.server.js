5b090834e182e0ee597a0748771ac26404aaeb76
siddarth kotthur <kotthursiddarth@gmail.com>
2023-07-27 21:01:15 -0500

    changed server page

--- src/routes/[universityName]/[universityCourse]/+page.server.js
+++ src/routes/[universityName]/[universityCourse]/+page.server.js
@@ -1,10 +1,13 @@
 export const load = async ({ fetch, params, depends }) => {
   depends("course-posts:load-posts");
-  const university = params.universityName;
-  const course = params.universityCourse;
+  const getAllPosts = async () => {
+    const university = params.universityName;
+    const course = params.universityCourse;
 
-  const response = await fetch(`/api/posts?university=${university}&course=${course}`);
-  const data = await response.json();
+    const response = await fetch(`/api/posts?university=${university}&course=${course}`);
+    const data = await response.json();
 
-  return { data };
+    return data;
+  };
+  return { data: getAllPosts() };
 };
