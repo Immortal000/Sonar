59d4bf40ab3740256c91050398f3e5d9f8355cae
WarrenWu4 <warrenweiwu04@gmail.com>
2023-07-31 19:21:12 -0500

    AHHHH

--- src/lib/components/Navigation_bar.svelte
+++ src/lib/components/Navigation_bar.svelte
@@ -42,7 +42,11 @@
           class="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0"
           on:click={() => (pop_down_triggered = !pop_down_triggered)}
         >
-          <img src={$page.data.session.user.picture} alt="profile pic" class="w-10 h-10 rounded-full" />
+          <img
+            src={$page.data.session.user.picture}
+            alt="profile pic"
+            class="w-10 h-10 rounded-full"
+          />
         </button>
       </div>
       <div
@@ -51,11 +55,15 @@
       >
         <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
           <div>{$page.data.session.user.name}</div>
-          <div class="font-medium truncate">{$page.data.session.user.email}</div>
+          <div class="font-medium truncate">
+            {$page.data.session.user.email}
+          </div>
         </div>
         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200">
           <li>
-            <a href="/profile" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            <a
+              href={`/profile/${$page.data.session.user.sub}`}
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
               >Profile</a
             >
           </li>
---
+++ src/routes/profile/[user_id]/+page.svelte
@@ -0,0 +1,3 @@
+<div class="max-w-[23.75rem] w-full h-full flex justify-center">
+  <div class="mt-[56px] w-[17rem] h-[25.875rem] bg-black" />
+</div>
