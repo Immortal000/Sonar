3ce2469261bb575792d20887e8272fb8fd6bf4f2
Redninjale <redninjale@gmail.com>
2023-08-02 15:01:56 -0500

    Homepage funcitonality

--- src/lib/components/Search_bar.svelte
+++ src/lib/components/Search_bar.svelte
@@ -5,11 +5,13 @@
   $: searchable_options = [];
 
   let search_value = "";
-  let searched_university = null;
+  export let searched_university = null;
 
   const handle_university_search = () => {
     const filtered_universities = all_unis.filter((value) => {
-      const name_matches = value.name.toLowerCase().includes(search_value.toLowerCase());
+      const name_matches = value.name
+        .toLowerCase()
+        .includes(search_value.toLowerCase());
 
       return name_matches;
     });
@@ -18,38 +20,45 @@
   };
 
   const handle_course_search = () => {
-    const matched_courses = searched_university.courses.filter((course) => course.toLowerCase().includes(search_value));
+    const matched_courses = searched_university.courses.filter((course) =>
+      course.toLowerCase().includes(search_value)
+    );
 
     searchable_options = matched_courses;
   };
 </script>
 
-<input
-  type="text"
-  bind:value={search_value}
-  on:input={() => {
-    if (!!searched_university) {
-      handle_course_search();
-    } else {
-      handle_university_search();
-    }
-  }}
-  placeholder={!!searched_university ? "Search Courses" : "Search Universities"}
-/>
-<div class="absolute divide-y divide-gray-500">
-  {#each searchable_options as option}
-    <option
-      value={option.name}
-      on:click={() => {
-        searched_university = option;
-        search_value = "";
-        searchable_options = [];
-      }}
-      >{#if !!searched_university}
-        {option}
-      {:else}
-        {option.name}
-      {/if}</option
-    >
-  {/each}
+<div class="border-2 border-gray-300 rounded-2xl w-full">
+  <input
+    type="text"
+    bind:value={search_value}
+    on:input={() => {
+      if (!!searched_university) {
+        handle_course_search();
+      } else {
+        handle_university_search();
+      }
+    }}
+    placeholder={!!searched_university
+      ? "search for a course"
+      : "search for a university"}
+    class="w-full px-2 border-2 rounded-2xl"
+  />
+  <div class="absolute divide-y divide-gray-500">
+    {#each searchable_options as option}
+      <option
+        value={option.name}
+        on:click={() => {
+          searched_university = option;
+          search_value = "";
+          searchable_options = [];
+        }}
+        >{#if !!searched_university}
+          {option}
+        {:else}
+          {option.name}
+        {/if}</option
+      >
+    {/each}
+  </div>
 </div>
