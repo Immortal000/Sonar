826a8cbfc7a36f9a83c8c1544265d2ac4adfb615
siddarth kotthur <kotthursiddarth@gmail.com>
2023-08-02 15:15:40 -0500

    search bar optimization

--- src/lib/components/Search_bar.svelte
+++ src/lib/components/Search_bar.svelte
@@ -8,21 +8,21 @@
   export let searched_university = null;
 
   const handle_university_search = () => {
-    const filtered_universities = all_unis.filter((value) => {
-      const name_matches = value.name
-        .toLowerCase()
-        .includes(search_value.toLowerCase());
+    if (search_value == "") {
+      searchable_options = [];
+    } else {
+      const filtered_universities = all_unis.filter((value) => {
+        const name_matches = value.name.toLowerCase().includes(search_value.toLowerCase());
 
-      return name_matches;
-    });
+        return name_matches;
+      });
 
-    searchable_options = filtered_universities;
+      searchable_options = filtered_universities;
+    }
   };
 
   const handle_course_search = () => {
-    const matched_courses = searched_university.courses.filter((course) =>
-      course.toLowerCase().includes(search_value)
-    );
+    const matched_courses = searched_university.courses.filter((course) => course.toLowerCase().includes(search_value));
 
     searchable_options = matched_courses;
   };
@@ -39,19 +39,17 @@
         handle_university_search();
       }
     }}
-    placeholder={!!searched_university
-      ? "search for a course"
-      : "search for a university"}
+    placeholder={!!searched_university ? "search for a course" : "search for a university"}
     class="w-full px-2 border-2 rounded-2xl"
   />
-  <div class="absolute divide-y divide-gray-500">
+  <div class="relative divide-y divide-gray-500 w-full">
     {#each searchable_options as option}
       <option
         value={option.name}
         on:click={() => {
           searched_university = option;
           search_value = "";
-          searchable_options = [];
+          searchable_options = searched_university.courses;
         }}
         >{#if !!searched_university}
           {option}
