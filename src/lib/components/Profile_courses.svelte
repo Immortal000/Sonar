3aab0435c5fe0dcbbbf62989a0272c3dcbec68bd
Redninjale <redninjale@gmail.com>
2023-11-02 09:41:24 -0500

    Merging

---
+++ src/lib/components/Profile_courses.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let userName;
+    export let userCourses
+</script>
+
+<div class="flex flex-col ">
+    <h1>{userName} Course's</h1>
+    {#each userCourses as course}
+        <span>{course}</span>
+    {/each}
+</div>
