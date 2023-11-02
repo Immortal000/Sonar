7ced1e86f0bf7eec9d7c70e9de6a3febe2359e82
Redninjale <redninjale@gmail.com>
2023-11-02 10:58:05 -0500

    Homepage and tailwind colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-primary': '#457B9D',
+        'light-primary': '#d9e3e9',
+        'light-text' : '#0E0B07',
+        'white-primary': '#F9F7F3',
+        'light-bg' : '#F9F7F3',
+        'filler' : '#7e7e7e',
+        'light-accent' : '#3f5274',
+        'light-light-accent' : '#c1cacc',
+      }
+    },
   },
   plugins: [],
 };
